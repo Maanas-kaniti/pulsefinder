67169320f49d4d7032236450c62db839a41021d7
DemonicEmperor <kanitimaanas@gmail.com>
2025-10-26 19:25:28 +0530

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
